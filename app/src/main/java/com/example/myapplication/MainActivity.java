50d9736d4741b9208a966e0bcaaa321fa8032785
RBen12 <romano.brentani@ict-campus.net>
2020-05-15 14:57:46 +0200

    init

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,31 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInitNewGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnInitNewGame = findViewById(R.id.btnInitnewGame);
+
+        View.OnClickListener toGameField = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+
 
     }
+
+
+
+
+
 }
