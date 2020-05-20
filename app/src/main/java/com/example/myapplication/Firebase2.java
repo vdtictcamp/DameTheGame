0a74554ec96c735dcf156c3348ca762ab36eeb1b
try776 <58332039+try776@users.noreply.github.com>
2020-05-20 08:49:43 +0200

    firebase finish

---
+++ app/src/main/java/com/example/myapplication/Firebase2.java
@@ -0,0 +1,129 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Firebase2 extends AppCompatActivity {
+
+    ListView listView;
+    Button button;
+
+    List<String> roomsList;
+
+    String playerName = "";
+    String roomName = "";
+
+    FirebaseDatabase database;
+    DatabaseReference roomRef;
+    DatabaseReference roomsRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase2);
+
+        database = FirebaseDatabase.getInstance();
+
+        //player name erhalten und diesem dem room zuweisen
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        playerName = preferences.getString("playerName", "");
+        roomName = playerName;
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.btnRoomCreate);
+
+        //alle verfügbare räume
+        roomsList = new ArrayList<>();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //room erstellen und selbst als player1 hinzufügen
+                button.setText("CREATING ROOM");
+                button.setEnabled(false);
+                roomName = playerName;
+                roomRef = database.getReference("rooms/" + roomName + "/player1");
+                addRoomEventListener();
+                roomRef.setValue(playerName);
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //bestehendem room beitreten und sich selbst als player2 hinzufügen
+                roomName = roomsList.get(position);
+                roomRef = database.getReference("rooms/" + roomName + "/player2");
+                addRoomEventListener();
+                roomRef.setValue(playerName);
+            }
+        });
+
+        //neue Räume anzeigen
+        addRoomsEventListener();
+    }
+
+    private void addRoomEventListener(){
+        roomRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //join room
+                button.setText("CREATE ROOM");
+                button.setEnabled(true);
+                Intent intent = new Intent(getApplicationContext(), Firebase3.class);
+                intent.putExtra("roomName", roomName);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //error
+                button.setText("CREATE ROOM");
+                button.setEnabled(true);
+                Toast.makeText(Firebase2.this, "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    private void addRoomsEventListener(){
+        roomsRef = database.getReference("rooms");
+        roomsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //rooms liste anzeigen
+                roomsList.clear();
+                Iterable<DataSnapshot> rooms = dataSnapshot.getChildren();
+                for (DataSnapshot snapshot : rooms){
+                    roomsList.add(snapshot.getKey());
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(Firebase2.this, android.R.layout.simple_list_item_1, roomsList);
+                    listView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // fehler, nicht ausführen
+
+            }
+        });
+    }
+}
