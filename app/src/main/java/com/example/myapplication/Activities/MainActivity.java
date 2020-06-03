d66c49cec04ab23956557ec87cb2c861ada6901d
SrijaanS <sockalingasr@ict-campus.net>
2020-06-03 11:03:14 +0200

    Design

--- app/src/main/java/com/example/myapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/MainActivity.java
@@ -6,21 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.os.Looper;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-
 import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
