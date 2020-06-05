699f195d6fb5073b1eab05fb79a948a94fdb1792
try776 <58332039+try776@users.noreply.github.com>
2020-06-05 15:14:48 +0200

    Transaction hashmap

---
+++ app/src/main/java/com/example/myapplication/Activities/Transaction.java
@@ -0,0 +1,17 @@
+package com.example.myapplication.Activities;
+
+public class Transaction {
+    public String colPos;
+    public String rowPos;
+    public String colStone;
+    public String rowStone;
+
+    public Transaction(String cP, String rP, String cS, String rS){
+        this.colPos = cP;
+        this.rowPos = rP;
+        this.colStone = cS;
+        this.rowStone = rS;
+    }
+
+
+}
